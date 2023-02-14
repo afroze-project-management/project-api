26ef795e32c06d8594e6c5d76e116dbf1f7046ed
afroze9 <afrozeamjad@gmail.com>
2023-02-14 12:31:45 +0500

    added task-definition.json to project-api

--- src/main/java/com/afroze/projectmanagement/project/api/ProjectApiApplication.java
+++ src/main/java/com/afroze/projectmanagement/project/api/ProjectApiApplication.java
@@ -2,10 +2,32 @@ package com.afroze.projectmanagement.project.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class ProjectApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApiApplication.class, args);
+	}
+
+	@Bean
+	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+		EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+		String ip = null;
+		try {
+			ip = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+
+		config.setIpAddress(ip);
+		config.setPreferIpAddress(true);
+
+		return config;
 	}
 }
